b5a6c8f9f01a309459d9337647a1aa32e94b2c8a
Yordanos <yordanos.yirgu1@gmail.com>
2024-12-24 20:38:57 +0300

    fix project setup

--- routes/students.js
+++
@@ -1,9 +0,0 @@
-import { createStudent, getStudents } from "../controllers/studentController";
-import { Router } from "express";
-
-const router = Router();
-
-router.post('/', createStudent);
-router.get('/', getStudents);
-
-export default router;
